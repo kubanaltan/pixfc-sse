0292aa83f96d87a1aa0df152c2334c824627f3ce
pixelfc@gmail.com <pixelfc@gmail.com@1c64e79a-1f5e-ae51-cc15-efd862a25ce1>
2011-06-22 00:00:23 +0000

    util: added release source script

---
+++ util/release/release-src.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check arguments
+if [ $# -ne 2 ]; then
+	echo "$0 <pixfc dir> <version tag>"
+	exit 1
+fi
+
+if [ ! -d "$1" ]; then
+	echo "$1 is not a directory"
+	exit 1
+fi
+
+release_name=pixfc-sse-v$2
+archive_name=${release_name}_src.zip
+
+# create temp dir
+rm -Rf temp
+mkdir temp
+cd temp
+
+# export working copy
+svn export "../$1" "${release_name}/"
+if [ $? -ne 0 ]; then
+	echo "Error exporting working copy"
+	exit 1
+fi
+
+# zip archive
+zip -r9 ../${archive_name} ${release_name}
+cd ..
+
+rm -Rf temp
+
